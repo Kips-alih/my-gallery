a54003ee0d6cc3e1871711cb7b741a8c032c0f6e
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-26 23:10:41 +0300

    Create Image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.TextField()
+    #image field
+    image = CloudinaryField('image')
