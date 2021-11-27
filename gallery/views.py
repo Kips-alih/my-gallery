ce53fbf2435b20ca64f36de2628111dfa90d291b
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-27 17:34:35 +0300

    Create search results view function

--- gallery/views.py
+++ gallery/views.py
@@ -14,3 +14,17 @@ def index(request):
     photo = Image.objects.all()
     return render(request, 'all-photos/index.html',{'photo':photo})
 
+#search results functions
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photos/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photos/search.html',{"message":message})
+
