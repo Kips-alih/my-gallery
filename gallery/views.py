75f6d88f6f8774e341209b2d8e6b1e31d13c6e22
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-27 17:32:51 +0300

    Define the index view function

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,16 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http  import HttpResponse
+
+from gallery.models import Image
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+def index(request):
+    # imports photos and save it in database
+    photo = Image.objects.all()
+    return render(request, 'all-photos/index.html',{'photo':photo})
+
