5c765e11ea360d92827ddd57e4b10d05ff200fdf
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-27 23:43:55 +0300

    Create category model

--- gallery/models.py
+++ gallery/models.py
@@ -3,13 +3,25 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
+
+class category(models.Model):
+    category=models.CharField(max_length=30)
+    
+    
+    def __str__(self):
+        return self.category
+
+
 class Image(models.Model):
     title = models.CharField(max_length=100)
     description= models.TextField()
     #image field
     image = CloudinaryField('image')
+    category = models.ForeignKey(category, on_delete=models.CASCADE,null=True)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
 
     @classmethod
     def search_by_title(cls,search_term):
         gallery = cls.objects.filter(title__icontains=search_term)
         return gallery
+    
