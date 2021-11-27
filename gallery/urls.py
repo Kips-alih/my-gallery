6e57dc1ddfaff53efbc1513dcfc789788731a5cb
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-27 17:36:50 +0300

    Define a single image view function

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     url('^$', views.index, name='index'),
     url(r'^search/', views.search_results, name='search_results'),
-    
+    url(r'^photo/(\d+)',views.photo,name ='photo')
 ]
