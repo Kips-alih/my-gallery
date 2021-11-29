cf260d05efaab6fa6023974ba00cd85108958fe2
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-29 06:31:18 +0300

    Modify the files

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Image,category
 
 # Create your tests here.
+# Testing Save Method
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.image=Image( title= 'Nature', description ='Our work to conserve biodiversity focuses on Key Biodiversity Areas.', image ='a6rncpjr5ptr0zuealce',category='nature',location='Kenya')
+
+    # Creating a new category and saving it
+        self.new_category = category(name = 'Nature')
+        self.new_category.save()
+
+        self.image.category.add(self.new_category)
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        category.objects.all().delete()
+
+    
