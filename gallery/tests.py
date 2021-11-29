bf75acdac95edb5af7f495bf564c79cc2c1caf36
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-29 10:35:47 +0300

    Add the test instance and delete image tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Image,category
+from .models import Image, Location,category
 
 # Create your tests here.
 # Testing Save Method
@@ -7,21 +7,23 @@ class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.image=Image( title= 'Nature', description ='Our work to conserve biodiversity focuses on Key Biodiversity Areas.', image ='a6rncpjr5ptr0zuealce',category='nature',location='Kenya')
+        self.image=Image( title= 'Nature', description ='Our work to conserve biodiversity focuses on Key Biodiversity Areas.', image ='http://image.com/image.jpg',category=category.objects.create(name="nature"),location=Location.objects.create(name='Kenya'))
 
-    # Creating a new category and saving it
-        self.new_category = category(name = 'Nature')
-        self.new_category.save()
 
-        self.image.category.add(self.new_category)
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.image,Image))
+        self.assertTrue(isinstance(self.image,Image))   
 
     def test_save_method(self):
         self.image.save_image()
         images = Image.objects.all()
         self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image.save_image()
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
 
     def tearDown(self):
         Image.objects.all().delete()
